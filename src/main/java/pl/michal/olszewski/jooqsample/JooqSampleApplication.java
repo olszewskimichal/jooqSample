e719dffee4d6a79e34804cdbe5a8102ef0ca3fc6
Michał <olszewskimichal@outlook.com>
2017-11-30 20:16:14 +0100

    Inicjalizacja projektu SpringBoot z JOOQ

---
+++ src/main/java/pl/michal/olszewski/jooqsample/JooqSampleApplication.java
@@ -0,0 +1,12 @@
+package pl.michal.olszewski.jooqsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqSampleApplication.class, args);
+	}
+}
