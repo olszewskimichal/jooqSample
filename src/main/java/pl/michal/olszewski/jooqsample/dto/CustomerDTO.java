9753aa6b6e1cd335b16115608b1a0a66be7390a7
Michał <olszewskimichal@outlook.com>
2017-12-01 17:34:43 +0100

    warstwa transportowa

---
+++ src/main/java/pl/michal/olszewski/jooqsample/dto/CustomerDTO.java
@@ -0,0 +1,16 @@
+package pl.michal.olszewski.jooqsample.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO {
+  private Long id;
+  private Long email;
+  private Set<ProductDTO> products = new HashSet<>();
+}
